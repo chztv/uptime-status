dfa3bf07f0b02a5dd758215ee4dba015eae86ffe
Paul Chan <0571chz@gmail.com>
2021-12-21 09:40:21 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -16,20 +16,23 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm786322663-402cd609baf66832d847ef63',
-    'm786322674-0773b2c046e85a9cd3344aa8',
-    'm786322754-8bd9c8c0e7f6bc59ddc376e7',
-    'm786322758-45dc64ec941e47673f2f5bd1',
-    'm786336367-8105e54df5e10b1654fe5df7',
-    'm786336382-19b8bc68931082ae50ad13fd',
-    'm786344761-38ac7c80cfba3bff6b4ee9b0',
-    'm786442374-d8ecbfbf82ad151ba3cf94f6',
-    'm787511928-e4ce8aad127e7c28d714985b',
-    'm788502517-d91440d32f8ab5200e337433',
+    'm790115571-b593143903ef665fcec0b858',//Webhorizon 新加坡
+    'm790115275-bb466e6e7d7c52770b7f4dbf',//Vir圣何塞
+    'm790115282-9e927d0e5c3a60ebf24b4dc7',//Vir水牛城
+    'm790115279-b783391a01df04d6f6c2e826',//Vir西雅图
+    'm782863226-cd42fbef7fa1e036307ab229',//One法国
+    'm790113352-1c6b4109bd914c9a820dfe88',//AWS日本
+    'm781209163-810fafc32b5a705d5c54dc47',//BWG日本
+    'm781209163-810fafc32b5a705d5c54dc47',//HH香港
+    'm782863238-198dcab55c8838864b66fb1c',//DMiT香港
+    'm790113394-045bb7228eeafd702d12185d',//CMI香港
+    'm790113387-6db8a9a689f0b43635cb6399',//绿云新加坡
+    'm790115245-50c06fee7ab81d42a9be3de7',//DO新加坡
+    'm790114718-16317deec25e177e1444254e',//Dedipath LA
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
